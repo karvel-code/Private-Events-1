87d90ff3137b66588734012af7f6eb3b8aeb58c6
verissimohenry <verissimohenry04@gmail.com>
2021-05-22 19:58:21 +0100

    add invite and user spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context ' check name validations' do
+    it 'return false if the name field is not filled in' do
+      u = User.new(name: '')
+      u.save
+      expect(u).to_not be_valid
+    end
+
+    it 'return true if the name field is filled in' do
+      u = User.new(name: 'sarah')
+      u.save
+      expect(u).to be_valid
+    end
+  end
 end
